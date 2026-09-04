5b899550fae34bfd72885fddcc6eb4510d69f3ff
Dilip9 <dilip.verma940805@gmail.com>
2026-09-05 00:31:50 +0530

    Monotonic stack problem

--- src/main/java/gouri/dsa/stack/StackProblem.java
+++ src/main/java/gouri/dsa/stack/StackProblem.java
@@ -1,5 +1,6 @@
 package gouri.dsa.stack;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -17,8 +18,11 @@ public class StackProblem {
          *
          * **/
         int[] arr = {4, 5, 2, 10, 8};
-        int[] result = nextGreaterElement(arr);
-        Arrays.stream(result).forEach(System.out::println);
+        int[] greaterElement = nextGreaterElement(arr);
+        int[] smallerElement = nextSmallerElement(arr);
+        Arrays.stream(greaterElement).forEach(n -> System.out.print(n+" "));
+        System.out.println();
+        Arrays.stream(smallerElement).forEach(n -> System.out.print(n+" "));
     }
     public static int[] nextGreaterElement(int[] arr){
         int[] answer = new int[arr.length];
@@ -32,5 +36,22 @@ public class StackProblem {
             stack.push(arr[i]);
         }
         return answer;
+    }
+
+
+    public static int[] nextSmallerElement(int[] arr){
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack();
+
+        for(int i=n-1; i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
+                stack.pop();
+            }
+            result[i]= stack.isEmpty() ? -1 : stack.peek();
+            stack.push(arr[i]);
+        }
+        return result;
+
     }
 }
